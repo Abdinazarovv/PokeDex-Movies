7e4d9d4f10c02a500ddcda652122f1d00da7e33e
Abdinazarovv <islombekabdinazarov07@gmail.com>
2022-07-24 19:54:49 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+let elList = document.querySelector(".list");
+let elBtn1 = document.querySelector(".btn__1");
+let elBtn2 = document.querySelector(".btn__2");
+let elBtnClean = document.querySelector(".btn__clean");
+let elResult = document.querySelector(".result");
+
+let pokemonArray = pokemons;
+let moviesArray = movies.slice(0, 151);
+
+elBtn1.addEventListener("click", function () {
+    elList.innerHTML = null
+    render(pokemonArray)
+})
+
+elBtn2.addEventListener("click", function () {
+    elList.innerHTML = null
+    render(moviesArray)
+})
+
+elBtnClean.addEventListener("click", function () {
+    elList.innerHTML = null
+    elList.innerHTML = `<li class="list__item text-center m-0">Tugamalardan birini bosing!</li>`
+    elResult.textContent = "Cleaned"
+})
+
+function render(array) {
+    array = array.slice(0, prompt("soni kiriting"));
+    elResult.textContent = array.length
+    for (const item of array) {
+        let newLi = document.createElement("li");
+        newLi.classList = "list__item"
+        
+        let newImg = document.createElement("img");
+        let newH3 = document.createElement("h3");
+        newImg.width = "200"
+        
+        if (item.name) {
+            newImg.src = item.img;
+            newH3.textContent = item.name;
+        }else {
+            newH3.textContent = item.Title;
+            newImg.src = `https://img.youtube.com/vi/${item.ytid}/mqdefault.jpg`;
+        }
+        
+        newLi.appendChild(newImg);
+        newLi.appendChild(newH3);
+        
+        elList.appendChild(newLi);
+    }
+}
